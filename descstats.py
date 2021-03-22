636ebd465db62ae41086104ce33fa33dfb6e7049
James Belleau <jpbelleau@gmail.com>
2021-03-22 08:10:35 -0500

    Added run explination and final calculation

--- descstats.py
+++ descstats.py
@@ -15,17 +15,54 @@ class DescStats:
         self.medians = []
 
     def outfmt(self, alist):
+        # Format the output
         alist = list(map(lambda x: round(x, 3), alist))
         return map(str, alist)
+
+    def calc(self):
+        ########## Mean
+        # Find the mean and count of incoming data
+        self.means.append(np.mean(self.data))
+        self.weights.append(len(self.data))
+
+        # Find the weighted average of the means
+        mmeans = np.average(self.means, weights=self.weights)
+
+        ########## Std dev
+        # Std dev we treat a little different
+        if self.batch_size == 1:
+            # If batch size of one, run for all of the means
+            # Which are really individual samples
+            mstddevs = np.std(self.means)
+        else:
+            # Find the variances of incoming data
+            self.vardevs.append(np.var(self.data))
+
+            # Find the weighted average of the variances and get the sq root (stddev)
+            mstddevs = sqrt(np.average(self.vardevs, weights=self.weights))
+
+        ########## Median
+        # Concatenate current medians with incoming
+        cmbmedians = np.concatenate((self.data, self.medians))
+
+        # Determine the local median and keep it
+        self.medians.append(np.median(self.data))
+
+        # Find the median of medians
+        mmedians = np.median(cmbmedians)
+
+        ########## Output
+        # Output and clear data list
+        res = [mmeans, mstddevs, mmedians]
+        print(",".join(self.outfmt(res)))
+        self.data = []
 
     def run(self):
         # Reading incoming numbers
         for incline in sys.stdin:
             incline = incline.rstrip()
 
-            # Time to exit
-            # Any accumulated data not in a batch
-            # is discarded
+            # Not expecting anymore data
             if incline == "QQ":
                 break
 
@@ -40,43 +77,12 @@ class DescStats:
 
             # Calculate and output
             if len(self.data) % self.batch_size == 0:
-                ########## Mean
-                # Find the mean and count of incoming data
-                self.means.append(np.mean(self.data))
-                self.weights.append(len(self.data))
-
-                # Find the weighted average of the means
-                mmeans = np.average(self.means, weights=self.weights)
-
-                ########## Std dev
-                # Std dev we treat a little different
-                if self.batch_size == 1:
-                    # If batch size of one, run for all of the means
-                    # Which are really individual samples
-                    mstddevs = np.std(self.means)
-                else:
-                    # Find the variances of incoming data
-                    self.vardevs.append(np.var(self.data))
-
-                    # Find the weighted average of the variances and get the sq root (stddev)
-                    mstddevs = sqrt(np.average(self.vardevs, weights=self.weights))
-
-                ########## Median
-                # Concatenate current medians with incoming
-                cmbmedians = np.concatenate((self.data, self.medians))
-
-                # Determine the local median and keep it
-                self.medians.append(np.median(self.data))
-
-                # Find the median of medians
-                mmedians = np.median(cmbmedians)
-
-                ########## Output
-                # Output and clear data list
-                res = [mmeans, mstddevs, mmedians]
-                print(",".join(self.outfmt(res)))
-                self.data = []
-
+                # Calculate!
+                self.calc()
+        
+        if len(self.data) != 0:
+            # Calculate the rest of the data
+            self.calc()
 
 def get_batch_size(batchsz):
     # Grab batch size
